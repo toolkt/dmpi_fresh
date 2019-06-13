6de8577603a2d7efb11538f29c737fd7ae286e22
klguial <lguial09@gmail.com>
2019-06-13 15:55:28 +0800

    [MOD] pricelist filename

--- dmpi_crm/models/dmpi_crm.py
+++ dmpi_crm/models/dmpi_crm.py
@@ -555,7 +555,8 @@ class DmpiCrmProductPriceList(models.Model):
 		print('send to sap pricelist')
 		for rec in self:
 
-			filename = 'ODOOPriceUploadZPR8_%s_%s.csv' % (rec.name.translate(removeWhiteSpace),datetime.now().strftime("%Y%m%d_%H%M%S"))
+			# filename = 'ODOOPriceUploadZPR8_%s_%s.csv' % (rec.name.translate(removeWhiteSpace),datetime.now().strftime("%Y%m%d_%H%M%S"))
+			filename = 'PriceUpZPR8_%s_%s.csv' % (rec.id, datetime.now().strftime("%Y%m%d_%H%M%S"))
 			path = '/tmp/%s' % filename
 
 			query = """
