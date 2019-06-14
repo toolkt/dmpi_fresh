152aabdc0832ae5590753238ca7900aaba79509f
klguial <lguial09@gmail.com>
2019-06-14 15:54:23 +0800

    [MOD] shorten pricelist csv filename

--- dmpi_crm/models/dmpi_crm.py
+++ dmpi_crm/models/dmpi_crm.py
@@ -556,7 +556,7 @@ class DmpiCrmProductPriceList(models.Model):
 		for rec in self:
 
 			# filename = 'ODOOPriceUploadZPR8_%s_%s.csv' % (rec.name.translate(removeWhiteSpace),datetime.now().strftime("%Y%m%d_%H%M%S"))
-			filename = 'PriceUpZPR8_%s_%s.csv' % (rec.id, datetime.now().strftime("%Y%m%d_%H%M%S"))
+			filename = 'PRU%s_%s.csv' % (rec.id, datetime.now().strftime("%Y%m%d_%H%M%S"))
 			path = '/tmp/%s' % filename
 
 			query = """
