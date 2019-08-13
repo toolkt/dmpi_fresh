5f9f1603bf54db02b032705e9671c19d02a3d730
Toolkit <dev@toolkt.com>
2019-08-13 14:12:16 +0800

    Update new line instructions

--- dmpi_crm/models/dmpi_crm_sale.py
+++ dmpi_crm/models/dmpi_crm_sale.py
@@ -711,7 +711,7 @@ class DmpiCrmSaleOrder(models.Model):
                 if rec.instructions:
                     #TODO parse per 10 letters bleak by word.
                     #instructions = rec.instructions.split() 
-                    writer.writerow(['INSTRUCTIONS' % rec.instructions])
+                    writer.writerow(['INSTRUCTIONS'])
                     writer.writerow(['%s' % rec.instructions])
 
             #TRANSFER TO REMOTE SERVER
